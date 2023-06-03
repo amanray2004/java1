b5110f7b3314096375ea58ff3216f6a7af90d1e2
amanray2004 <119524133+amanray2004@users.noreply.github.com>
2023-06-03 13:29:32 +0000

    1

--- sorting methods/mergesort.c
+++ sorting methods/mergesort.c
@@ -1,54 +1,51 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-
-void merge(int ls,int re,int arr[],int mid){
-  int sorted[re-ls+1];
-  int i=ls;
-  int j =mid+1;
-  int k=0;
-  while(i<=mid&&j<=re){
-    if (arr[i]<=arr[j]){
-      sorted[k]=arr[i];
+void merge(int ls, int re, int arr[], int mid) {
+  int sorted[re - ls + 1];
+  int i = ls;
+  int j = mid + 1;
+  int k = 0;
+  while (i <= mid && j <= re) {
+    if (arr[i] <= arr[j]) {
+      sorted[k] = arr[i];
       i++;
-      
-    }
-    else{
-      sorted[k]=arr[j];
+
+    } else {
+      sorted[k] = arr[j];
       j++;
     }
     k++;
-    
   }
-  while(i<=mid){
-    sorted[k]=arr[i];
+  while (i <= mid) {
+    sorted[k] = arr[i];
     i++;
     k++;
   }
-  while(j<=re){
-    sorted[k]=arr[j];
+  while (j <= re) {
+    sorted[k] = arr[j];
     j++;
     k++;
   }
-  for (int x=ls;x<=re;x++){
-    arr[x]=sorted[x-ls];
+  for (int x = ls; x <= re; x++) {
+    arr[x] = sorted[x - ls];
   }
 }
 
-void mergesort(int arr[],int ls,int re){
+void mergesort(int arr[], int ls, int re) {
   int mid;
-  if(ls<re){
-    mid=(ls+re)/2;
-    mergesort(arr,ls,mid);
-    mergesort(arr,mid+1,re);
-    merge(ls,re,arr,mid);
+  if (ls < re) {
+    mid = (ls + re) / 2;
+    mergesort(arr, ls, mid);
+    mergesort(arr, mid + 1, re);
+    merge(ls, re, arr, mid);
   }
 }
 
-int main(){
-  int arr[]={12,3,7,8,90,8766};
-  mergesort(arr,0,5);
-  for(int i=0;i<6;i++){
-    printf("%d   ",arr[i]);
+int main() {
+  int arr[] = {27, 12, 14, 20, 35, 33, 52, 59};
+  mergesort(arr, 0, 7);
+  for (int i = 0; i < 8; i++) {
+    printf("%d   ", arr[i]);
   }
 }
