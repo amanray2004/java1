c6395381f7be49006510c449f9d935421a240260
amanray2004 <119524133+amanray2004@users.noreply.github.com>
2023-06-14 19:33:51 +0000

    15 june 1:03

---
+++ .ccls-cache/@home@runner@java1/c@trees@iterativetraversals.c
@@ -0,0 +1,80 @@
+// iterative traversal
+
+// preorder
+
+// if(t!=null){
+// print()
+// push(t)
+// t=t->lchild
+// }
+
+// else if (t==NULL){
+// t=pop();
+// t=t->rchild;
+// }
+
+
+
+
+
+
+#include <stdio.h>
+#include<stdlib.h>
+#include "createtree.h"
+struct node * st=NULL;
+void pushstack(struct dnode* p) {
+    struct node* q = (struct node*)malloc(sizeof(struct node));
+    q->ptr = p;
+    q->next = st;
+    st = q;
+}
+
+struct dnode* popstack() {
+    if (st == NULL) {
+        return NULL;
+    } else {
+        struct node* p = st;
+        st = st->next;
+        struct dnode* poppedValue = p->ptr;
+        free(p);
+        return poppedValue;
+    }
+}
+
+int isemptyst(){
+  if(st==NULL){
+    return 1;
+  }
+  return 0;
+  
+}
+
+
+
+
+void preorderiterative(struct dnode * ptr){
+  struct dnode * t=ptr;
+  pushstack(t);
+  while(!isemptyst()){
+    if(t!=NULL){
+      printf("%d ",t->data);
+      pushstack(t);
+      t=t->lchild;
+    }
+
+    else if(t==NULL){
+      t=popstack();
+      t=t->rchild;
+      
+    }
+
+    
+  }
+}
+int main(void) {
+  createtree();
+  preorderiterative(root);
+  return 0;
+}
+
+
