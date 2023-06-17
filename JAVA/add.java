bb6aaf576711aca6332dbcd1f7123f2e3a00e52d
amanray2004 <119524133+amanray2004@users.noreply.github.com>
2023-06-17 08:21:45 +0530

    y

---
+++ JAVA/add.java
@@ -0,0 +1,10 @@
+package JAVA;
+
+import javax.sound.sampled.SourceDataLine;
+
+public class add {
+    public static void main(String[] args) {
+        int a=9;int b=55;
+        System.out.println(a+b);
+    }
+}
